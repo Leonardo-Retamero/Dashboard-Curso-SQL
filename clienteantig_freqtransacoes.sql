865834e87c8c0a6a5dd3057d331a5252a9024c42
Leonardo Retamero <leonardoretamero66@gmail.com>
2025-11-24 19:28:48 -0300

    Update clienteantig_freqtransacoes.sql

--- clienteantig_freqtransacoes.sql
+++ clienteantig_freqtransacoes.sql
@@ -3,7 +3,11 @@
 SELECT c.idCliente,
        CAST(julianday('now') - julianday(substr(c.DtCriacao, 1, 19)) AS INT) AS IdadeBase,
        COUNT(tr.IdTransacao) AS qtdeTransacoes
+       
 FROM clientes AS c
+       
 LEFT JOIN transacoes AS tr ON c.idCliente = tr.idCliente
+       
 GROUP BY c.idCliente, IdadeBase
+
 ORDER BY IdadeBase DESC
